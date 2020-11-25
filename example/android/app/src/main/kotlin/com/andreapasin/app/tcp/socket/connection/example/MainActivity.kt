7594dcbf77a9e433c0b8d4784076a7124628d920
Andrea <andrea.pasin99@gmail.com>
2020-11-25 18:06:16 +0100

    Adding example

---
+++ example/android/app/src/main/kotlin/com/andreapasin/app/tcp/socket/connection/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.andreapasin.app.tcp.socket.connection.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:tcp_socket_connection/tcp_socket_connection.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  //Instantiating the class with the Ip and the PortNumber
+  TcpSocketConnection socketConnection=TcpSocketConnection("192.168.1.113", 10251);
+  String message="";
+
+  @override
+  void initState() {
+    super.initState();
+    startConnection();
+  }
+
+  //receiving and sending back a custom message
+  void messageReceived(String msg){
+    setState(() {
+      message=msg;
+    });
+    socketConnection.sendMessage("MessageIsReceived :D");
+  }
+
+  //starting the connection and listening to the socket asynchronously
+  void startConnection() async{
+    await socketConnection.connect(5000, "EOF", messageReceived);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text(
+          'You have received '+ message,
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
